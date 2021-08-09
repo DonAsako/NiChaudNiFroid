1f765e06959a63be8748833edf263abea0a04f4a
DonAsako <asakosan.pro@protonmail.com>
2021-08-09 19:15:25 +0200

    :sparkles: add current_scene

--- src/game.py
+++ src/game.py
@@ -1,10 +1,11 @@
 import pygame
+from src.scenes.room import RoomScene
 
 
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((720, 720))
-        self.current_scene = None
+        self.current_scene = RoomScene(self)
         self.is_running = False
         self.clock = pygame.time.Clock()
 
@@ -17,6 +18,8 @@ class Game:
         self.dt = self.clock.tick(60)
         self.handle_event()
         self.screen.fill((0, 0, 0))
+        self.current_scene.update()
+        self.current_scene.draw()
         pygame.display.flip()
 
     def handle_event(self):
