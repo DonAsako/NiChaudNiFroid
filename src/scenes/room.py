dd803e442df12a6afa4882667fb23d8b07a8724e
DonAsako <asakosan.pro@protonmail.com>
2021-08-10 13:22:22 +0200

    :sparkles: add music && text score

--- src/scenes/room.py
+++ src/scenes/room.py
@@ -9,6 +9,7 @@ class RoomScene(Scene):
         super().__init__(game)
         self.screen = self.game.screen
         self.player = Player()
+        self.font = pygame.font.Font("assets/font/Silver.ttf", 48)
 
         # BACKGROUND
         self.background_image = pygame.image.load("assets/image/background.png")
@@ -23,10 +24,18 @@ class RoomScene(Scene):
         self.background_rect.bottom = self.game.screen.get_height()
         self.background_rect_1 = self.background_rect.copy()
         self.background_rect_1.left = self.background_image.get_width()
+        # Score
+        self.score = 0
+        self.score_sound = pygame.mixer.Sound("assets/sound/score.wav")
+        # Music
+        pygame.mixer.music.load("assets/sound/music.wav")
+        pygame.mixer.music.play(-1)
 
     def update(self):
         self.handle_event()
         self.player.update(self.game.dt)
+
+        # Background
         vel = math.ceil(20 * (self.game.dt / 100))
         self.background_rect.left -= vel
         self.background_rect_1.left -= vel
@@ -38,6 +47,12 @@ class RoomScene(Scene):
             self.background_rect_1.left = (
                 self.background_image.get_width() + self.background_rect.left
             )
+
+        # HUD
+        self.text_score = self.font.render(
+            f"Score : {int(self.score)}", False, (255, 255, 255)
+        )
+        self.score += self.game.dt / 200
 
     def draw(self):
 
@@ -53,6 +68,9 @@ class RoomScene(Scene):
 
         # draw Player
         self.screen.blit(self.player.image, self.player.rect)
+
+        # draw Hud
+        self.screen.blit(self.text_score, self.text_score.get_rect())
 
     def handle_event(self):
         ...
