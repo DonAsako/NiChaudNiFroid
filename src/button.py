6e4f128858763d2091fda2affa0430ffccfb39db
DonAsako <asakosan.pro@protonmail.com>
2021-10-16 12:56:10 +0200

    :sparkles: new menu

---
+++ src/button.py
@@ -0,0 +1,93 @@
+from typing import Collection
+import pygame
+
+
+class Button(pygame.sprite.Sprite):
+    def __init__(
+        self,
+        size: Collection,
+        position: Collection,
+        text: str,
+        font: pygame.font.Font,
+        color: pygame.Color,
+        shadow_color: pygame.Color,
+        hover_color: pygame.Color,
+        text_color: pygame.Color,
+        on_pressed: None,
+    ):
+        pygame.sprite.Sprite.__init__(self)
+        self.size = size
+        self.position = position
+        self.text = text
+        self.font = font
+        self.color = color
+        self.shadow_color = shadow_color
+        self.hover_color = hover_color
+        self.text_color = text_color
+        self.on_pressed = on_pressed
+        self.image = pygame.Surface(self.size)
+        self.rect = self.image.get_rect()
+        self.rect.center = self.position
+        self.image.set_colorkey((0, 0, 0, 0))
+
+        # Button Draw
+        pygame.draw.rect(
+            self.image,
+            color=self.shadow_color,
+            rect=[0, 0, self.size[0], self.size[1]],
+            width=0,
+            border_radius=5,
+        )
+        pygame.draw.rect(
+            self.image,
+            color=self.color,
+            rect=[0, 5, self.size[0], self.size[1] - 5],
+            width=0,
+            border_radius=5,
+        )
+        # Title
+        self.title = self.font.render(self.text.upper(), True, self.text_color)
+        self.image.blit(
+            self.title,
+            (
+                self.rect.width / 2 - self.title.get_width() / 2,
+                self.rect.height / 2 - self.title.get_height() / 2 + 7.5,
+            ),
+        )
+
+    def update(self):
+        self.handle_event()
+
+    def handle_event(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if (
+            self.rect.top < mouse_pos[1] < self.rect.bottom
+            and self.rect.left < mouse_pos[0] < self.rect.right
+        ):
+            self.on_hover()
+            if pygame.mouse.get_pressed(3)[0]:
+                self.on_pressed()
+        else:
+            pygame.draw.rect(
+                self.image,
+                color=self.color,
+                rect=[0, 5, self.size[0], self.size[1] - 5],
+                width=0,
+                border_radius=5,
+            )
+        self.image.blit(
+            self.title,
+            (
+                self.rect.width / 2 - self.title.get_width() / 2,
+                self.rect.height / 2 - self.title.get_height() / 2 + 7.5,
+            ),
+        )
+
+    def on_hover(self):
+        pygame.draw.rect(
+            self.image,
+            color=self.hover_color,
+            rect=[0, 5, self.size[0], self.size[1] - 5],
+            width=0,
+            border_radius=5,
+        )
--- src/scenes/menu.py
+++ src/scenes/menu.py
@@ -1,6 +1,7 @@
-from src.scene import Scene
 import pygame
+from src.scene import Scene
 from src.player import Player
+from src.button import Button
 
 
 class MenuScene(Scene):
@@ -12,61 +13,68 @@ class MenuScene(Scene):
         # Title
         self.font = pygame.font.Font("assets/font/Silver.ttf", 192)
         self.title_image = self.font.render(
-            "Ni Chaud, Ni Froid", False, (200, 200, 200)
+            "Ni Chaud, Ni Froid", False, ((204, 109, 145, 0.5))
         )
         self.title_rect = self.title_image.get_rect()
         self.title_rect.center = (
             self.screen.get_width() / 2,
             self.row_height * 2,
         )
-
-        # Player
-        self.player_idle = pygame.image.load("assets/image/player_idle.png").convert()
-        self.player_image = self.get_image_animation_player()
-        self.player_rect = self.player_image.get_rect()
-        self.player_rect.center = (
-            self.screen.get_width() / 2,
-            self.row_height * 4,
+        self.button_font = pygame.font.Font("assets/font/Silver.ttf", 36)
+        self.play_button = Button(
+            size=[256, 64],
+            position=[
+                self.screen.get_width() / 2,
+                self.row_height * 4,
+            ],
+            font=self.button_font,
+            text="Play",
+            color=(204, 109, 145),
+            shadow_color=(255, 255, 255),
+            hover_color=(198, 161, 182),
+            text_color=(255, 255, 255),
+            on_pressed=lambda: self.game.change_scene("room"),
+        )
+        self.scores_button = Button(
+            size=[256, 64],
+            position=[
+                self.screen.get_width() / 2,
+                self.row_height * 5,
+            ],
+            font=self.button_font,
+            text="Scores",
+            color=(204, 109, 145),
+            shadow_color=(255, 255, 255),
+            hover_color=(198, 161, 182),
+            text_color=(255, 255, 255),
+            on_pressed=lambda: print("Scores"),
+        )
+        self.settings_button = Button(
+            size=[256, 64],
+            position=[
+                self.screen.get_width() / 2,
+                self.row_height * 6,
+            ],
+            font=self.button_font,
+            text="Settings",
+            color=(204, 109, 145),
+            shadow_color=(255, 255, 255),
+            hover_color=(198, 161, 182),
+            text_color=(255, 255, 255),
+            on_pressed=lambda: print("Settings"),
         )
 
-        # Button
-        self.space_button = pygame.image.load("assets/image/button_space.png").convert()
-        self.button_image = self.get_image_animation_space_key()
-        self.button_rect = self.button_image.get_rect()
-        self.button_rect.center = (
-            self.screen.get_width() / 2,
-            self.row_height * 6,
+        self.buttons = pygame.sprite.Group(
+            self.play_button, self.settings_button, self.scores_button
         )
 
     def update(self):
-        # Player Animation
-        self.player_image = self.get_image_animation_player()
-        self.index += 1 * (self.game.dt / 100)
-        if self.index >= 9:
-            self.index = 0
-
-        # Button Animation
-        self.button_image = self.get_image_animation_space_key()
+        self.buttons.update()
 
     def draw(self):
-        self.screen.fill((11, 11, 11))
+        self.screen.fill((255, 190, 200))
         self.screen.blit(self.title_image, self.title_rect)
-        self.screen.blit(self.player_image, self.player_rect)
-        self.screen.blit(self.button_image, self.button_rect)
-
-    def get_image_animation_player(self):
-        image = pygame.Surface((48, 48))
-        image.blit(self.player_idle, (0, 0), (int(self.index) * 48, 0, 48, 48))
-        image.set_colorkey((0, 0, 0))
-        return pygame.transform.scale(image, (48 * 4, 48 * 4))
-
-    def get_image_animation_space_key(self):
-        image = pygame.Surface((64, 32))
-        image.blit(
-            self.space_button, (0, 0), ((int(self.index / 2) % 2) * 64, 0, 64, 64)
-        )
-        image.set_colorkey((0, 0, 0))
-        return pygame.transform.scale(image, (64 * 4, 32 * 4))
+        self.buttons.draw(self.screen)
 
     def handle_event(self):
         if pygame.key.get_pressed()[pygame.K_SPACE]:
